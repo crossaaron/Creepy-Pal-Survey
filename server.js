794d5f6f62fd14cd4d065aac21769b86c9bf0c6b
Aaron Cross <aaroncross@Aarons-MBP.fios-router.home>
2018-02-04 22:44:57 -0500

    html pages routed to server

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var htmlRoutes = require('./routing/htmlRoutes.js');
+
+var port = process.env.PORT || 3000
+var app = express();
+
+app.listen(port, () => {
+    console.log('App is listening port ' + port);
+});
+
+app.use('/', htmlRoutes);
+app.use('/survey', htmlRoutes);
