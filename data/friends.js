189fad030f6eee5f075c36debdf324dfed2df78f
Aaron Cross <aaroncross@Aarons-MBP.fios-router.home>
2018-02-05 20:55:28 -0500

    get/post working for user survey submit

--- data/friends.js
+++ data/friends.js
@@ -1,8 +1,23 @@
+var userArr = []
 var answerArr = []
 
 $(surveySubmit).on("click", function () {
     for (i = 0; i < $("#survey")[0].length; i++) {
         answerArr.push(($("#q" + i).val()));
     }
-    console.log($(answerArr));
+    var newUser = {
+        name: $('#nameInput').val(),
+        url: $('#photoInput').val(),
+        surveyAnswers: answerArr
+    };
+    $.post("/api/new", newUser)
+        .then(function (data) {
+            console.log(data);
+            alert("Adding User");
+        });
 });
+
+
+
+
+  
--- server.js
+++ server.js
@@ -6,10 +6,35 @@ var htmlRoutes = require('./routing/htmlRoutes.js');
 var port = process.env.PORT || 3000
 var app = express();
 
+var users = [];
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.listen(port, () => {
     console.log('App is listening port ' + port);
+});
+
+
+// Create New Users - takes in JSON input
+app.post("/api/new", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newUser = req.body;
+    // Using a RegEx Pattern to remove spaces from newUser
+    newUser.routeName = newUser.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newUser);
+
+    users.push(newUser);
+
+    res.json(newUser);
+});
+
+app.get('/api/new', function (req, res) {
+    res.json(users);
 });
 
 app.use('/', htmlRoutes);
 app.use('/survey', htmlRoutes);
 app.use('/data/friends.js', htmlRoutes);
+
